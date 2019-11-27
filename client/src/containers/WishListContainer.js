addd8c070cf797347b36069d4ad27873965e35c8
Diana Badas <diana.badas@gmail.com>
2019-11-27 15:04:07 +0000

    Refactoring WishList, adding actions to interact with db

--- client/src/containers/WishListContainer.js
+++ client/src/containers/WishListContainer.js
@@ -1,18 +1,58 @@
 import React, { PureComponent } from 'react';
 import '../css/BookListContainer.css';
 import { connect } from 'react-redux';
-import WishList from '../components/WishList';
 import { deleteBook } from '../actions/wishList';
+import { getMyWishList } from '../actions/wishList';
+import { Link } from 'react-router-dom';
+
 
 class WishListContainer extends PureComponent {
-    renderBooks = () => {
-        return this.props.books.map( book => <WishList deleteBook={this.props.deleteBook} key={book.id} book={book} />)
-    }
+
+    handleDeleteBook = (book) => {
+        console.log(book)
+        this.props.deleteBook(book);
+        window.location.reload();
+    };
+    
+    componentDidMount() {
+        this.props.getMyWishList();
+    };
+
+
+    handleLoading = () => {
+        const books = this.props.books.length > 0 ? 
+        this.props.books.map( book => (
+
+            <div>
+                <span>
+                    <img
+                        src={ book.image === "" || book.image === null ? '/download.jpeg' : book.image }
+                        alt={ book.title }
+                    />
+                    {/*Passing props with Link react-router to `/wishlist/books/:id` that is connected to Book component*/}
+                    <Link to={{
+                        pathname: `/wishlist/books/${book.title}`,
+                        data: book
+                    }} ><h3>{ book.title }</h3></Link>
+                    <p>by { book.author }</p>
+                    <p>More Info: { book.info } </p>
+                    
+                    <p> <button onClick={ () =>  this.handleDeleteBook( book.id )}>Click here to delete from list</button></p>
+                    
+                </span>
+            
+            </div>
+        ))
+        : null
+        return books
+        
+    };
+    
     render () {
         return (
             <div className="BookListContainer">
                 <h1>My wishlist</h1>
-                {this.renderBooks()}
+                 {this.handleLoading()}
             </div>
         );
     };
@@ -25,7 +65,9 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => {
-    return { deleteBook: book => dispatch(deleteBook(book)) }
+    return { 
+        getMyWishList: () => dispatch(getMyWishList()),
+        deleteBook: book => dispatch(deleteBook(book)) }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(WishListContainer);
