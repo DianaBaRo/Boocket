799e9b09a79c63286365dead8662bf7b24478004
Diana Badas <diana.badas@gmail.com>
2019-11-27 15:08:42 +0000

    Deleting new releases route

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,9 +6,8 @@ const NavBar = () => {
     <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
         <NavLink style={{ marginRight: '10px' }}  to="/">Home</NavLink><br />
         <NavLink style={{ marginRight: '10px' }}  to="/search">Search</NavLink><br />
-        <NavLink style={{ marginRight: '10px' }}  to="/new-releases">New Releases</NavLink><br />
-        <NavLink style={{ marginRight: '10px' }} to="/wishlist" >Wishlist </NavLink><br />
-        <NavLink style={{ marginRight: '10px' }} to="/create-book" >Create book </NavLink>
+        <NavLink style={{ marginRight: '10px' }} to="/create-book" >Create book </NavLink><br />
+        <NavLink style={{ marginRight: '10px' }} to="/wishlist" >Wishlist </NavLink>
     </div>
   );
 };
