ff24213b1b78336cfd373c292d5ca7ca4b4f1848
Diana Badas <diana.badas@gmail.com>
2019-11-27 15:07:44 +0000

    Deleting LatestBookListContainer and its routes, adding wishlist/books/:title route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './NavBar';
 import SearchableBookListContainer from '../containers/SearchableBookListContainer';
-import LatestBookListContainer from '../containers/LatestBookListContainer';
 import Home from './Home';
 import NotFound from './NotFound';
 import Book from './Book';
@@ -15,11 +14,10 @@ const App = () => {
             <>
                 <NavBar />
                 <Switch>
-                    <Route path='/search/books/:id' component={ Book } />
+                    <Route path='/search/books/:title' component={ Book } />
                     <Route path='/search' component={ SearchableBookListContainer } />
-                    <Route path='/new-releases/books/:id' component={ Book} />
-                    <Route path='/new-releases' component={ LatestBookListContainer } />
                     <Route path='/create-book' component={ CreateBook } />
+                    <Route path='/wishlist/books/:title' component={ Book } />
                     <Route path='/wishlist' component={ WishListContainer } />
                     <Route exact path='/' component={ Home } />
                     <Route component={ NotFound } />
